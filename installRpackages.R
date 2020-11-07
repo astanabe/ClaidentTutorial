a3652b15e1590f7873edfe15f70041812c6300b7
Akifumi S. Tanabe <akifumi.tanabe+github@gmail.com>
2020-11-08 08:29:19 +0900

    Update R scripts

--- installRpackages.R
+++ installRpackages.R
@@ -1,1 +1,1 @@
-install.packages(c("vegan", "colorspace", "RColorBrewer", "tidyverse", "khroma", "picante", "bipartite", "geosphere"), repos = "http://cloud.r-project.org/", dependencies=T, clean=T)
+install.packages(c("vegan", "colorspace", "RColorBrewer", "tidyverse", "khroma", "picante", "bipartite", "geosphere", "foreach", "doParallel", "parallel", "mpmcorrelogram"), repos = "http://cloud.r-project.org/", dependencies=T, clean=T)
