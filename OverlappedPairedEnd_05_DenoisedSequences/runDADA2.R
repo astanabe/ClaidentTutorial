36569d8718871f1c4b414cbac1ec4cb4644fcf63
Akifumi S. Tanabe <akifumi.tanabe+github@gmail.com>
2021-03-12 00:54:03 +0900

    Add newly generated sequences and analysis results

---
+++ OverlappedPairedEnd_05_DenoisedSequences/runDADA2.R
@@ -0,0 +1,22 @@
+ranseed <- 1617905898
+numthreads <- 32
+outputfolder <- "OverlappedPairedEnd_05_DenoisedSequences"
+pooling <- T
+library(dada2)
+set.seed(ranseed)
+setDadaOpt(OMEGA_C=0)
+fn1 <- sort(list.files(outputfolder, pattern="\\.fastq$", full.names=T))
+extract.sample.names <- function (x) { sub("\\.fastq$", "", sub("^.*\\/", "", x)) }
+names(fn1) <- sapply(fn1, extract.sample.names)
+derep1 <- derepFastq(fn1, verbose=T, qualityType="FastqQuality")
+err1 <- learnErrors(derep1, verbose=T, multithread=numthreads, qualityType="FastqQuality")
+pdf(file=paste0(outputfolder, "/plotErrors.pdf"))
+plotErrors(err1, obs=T, err_out=T, err_in=T, nominalQ=T)
+dev.off()
+dada1 <- dada(derep1, err=err1, verbose=T, multithread=numthreads, pool=pooling)
+for (i in 1:length(fn1)) {
+    write.table(derep1[[i]]$map, paste0(outputfolder, "/", names(fn1)[i], "_derepmap.txt"), sep="\t", col.names=F, row.names=F, quote=F)
+    write.table(derep1[[i]]$uniques, paste0(outputfolder, "/", names(fn1)[i], "_uniques.txt"), sep="\t", col.names=F, row.names=T, quote=F)
+    write.table(dada1[[i]]$map, paste0(outputfolder, "/", names(fn1)[i], "_dadamap.txt"), sep="\t", col.names=F, row.names=F, quote=F)
+    write.table(dada1[[i]]$denoised, paste0(outputfolder, "/", names(fn1)[i], "_denoised.txt"), sep="\t", col.names=F, row.names=T, quote=F)
+}
