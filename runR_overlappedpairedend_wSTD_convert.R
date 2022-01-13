70c39ac5bc4297277baaa204157f456c106fa33a
Akifumi S. Tanabe <akifumi.tanabe+github@gmail.com>
2022-01-13 21:43:03 +0900

    Update results

--- runR_overlappedpairedend_wSTD_convert.R
+++ runR_overlappedpairedend_wSTD_convert.R
@@ -1,22 +1,41 @@
+library(doParallel)
+
 Community <- read.table("OverlappedPairedEnd_wSTD_11_ClaidentResults/sample_otu_matrix_fishes.tsv", header=T, row.names=1)
 Standard <- read.table("OverlappedPairedEnd_wSTD_11_ClaidentResults/sample_otu_matrix_standard.tsv", header=T, row.names=1)
+
+stopifnot(nrow(Community) == nrow(Standard))
 
 # Copy number per 1uL of internal standard
 StandardCopy <- c(10, 20, 40, 80)
 
 slope <- data.frame()
 # Function definition
+CalcSlope <- function(x){
+  return(lm(as.numeric(Standard[x,]) ~ StandardCopy + 0)$coefficients)
+}
 ConvertReads <- function(x){
-  slope[x,1] <<- lm(as.numeric(Standard[x,]) ~ StandardCopy + 0)$coefficients
   conv <- Community[x,] / slope[x,1]
   return(conv)
 }
 
+# Leastsquare regression
+slope <- data.frame()
+cl <- makeCluster(detectCores())
+registerDoParallel(cl)
+slope <- foreach(i = 1:nrow(Standard), .combine=rbind, .inorder=T, .export=c("Standard", "StandardCopy", "CalcSlope")) %dopar% {
+	CalcSlope(i)
+}
+stopCluster(cl)
+
 # Convert number of reads based on number of internal standard reads
 Converted <- data.frame()
-for(i in 1:nrow(Community)){
-  Converted <- rbind(Converted, ConvertReads(i))
+cl <- makeCluster(detectCores())
+registerDoParallel(cl)
+Converted <- foreach(i = 1:nrow(Community), .combine=rbind, .inorder=T, .export=c("Community", "ConvertReads", "slope")) %dopar% {
+	ConvertReads(i)
 }
+stopCluster(cl)
+
 Converted[is.na(Converted)] <- NA
 colnames(Converted) <- colnames(Community)
 # Converted to DNA copy number per 1L (Filtered water amount = 1L, DNA extract = 200uL)
