59cd4961cfef93f36a35ba83cfa462118ee76318
astanabe <akifumi.tanabe@gmail.com>
2024-06-15 14:01:50 +0900

    Result files have been updated.

--- installRpackages.R
+++ installRpackages.R
@@ -1,2 +1,2 @@
 library(parallel)
-install.packages(c("vegan", "colorspace", "RColorBrewer", "tidyverse", "ggsci", "khroma", "picante", "bipartite", "geosphere", "foreach", "doParallel", "mpmcorrelogram", "pvclust"), repos="http://cloud.r-project.org/", dependencies=T, clean=T, upgrade="never", Ncpus=detectCores())
+install.packages(c("vegan", "colorspace", "RColorBrewer", "tidyverse", "ggsci", "khroma", "picante", "bipartite", "geosphere", "foreach", "doParallel", "mpmcorrelogram", "pvclust"), repos="http://cloud.r-project.org/", clean=T, upgrade="never", Ncpus=detectCores())
