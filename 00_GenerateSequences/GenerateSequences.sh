566592e70155259fd5bc344cc4ae8e4c075b57e6
astanabe <akifumi.tanabe@gmail.com>
2024-08-24 19:12:35 +0900

    Sequence generation script has been updated.

--- 00_GenerateSequences/GenerateSequences.sh
+++ 00_GenerateSequences/GenerateSequences.sh
@@ -1,3 +1,27 @@
+# Install several packages
+sudo apt install libgsl-dev libmath-random-mt-perl python2.7
+# Install ecoPCR
+tar -xzf ecoPCR-0.8.0.tar.gz
+cd ecoPCR-0.8.0/src
+make -j4
+make ecoisundertaxon
+cp ecofind ecogrep ecoisundertaxon ecoPCR ../..
+cd ../tools
+cp *.py ../..
+cd ../..
+# Download and compile Simera
+wget -c -O Simera-master.tar.gz https://github.com/bnichols1979/Simera/archive/refs/heads/master.tar.gz
+tar -xzf Simera-master.tar.gz
+patch Simera-master/Simera.h < Simera.diff
+cd Simera-master
+make clean
+make
+cd ..
+# Download ART simulator
+wget -c https://www.niehs.nih.gov/research/resources/assets/docs/artbinmountrainier2016.06.05linux64.tgz
+# Extract ART simulator
+tar -xzf artbinmountrainier2016.06.05linux64.tgz
+
 # Download taxonomy dump file and extract
 mkdir -p taxonomy
 cd taxonomy
@@ -8,15 +32,6 @@ tar -xzf taxdump.tar.gz
 chmod 644 taxdump.tar.gz
 rm taxdump.tar.gz
 cd ..
-# Install ecoPCR
-tar -xzf ecoPCR-0.8.0.tar.gz
-cd ecoPCR-0.8.0/src
-make -j4
-make ecoisundertaxon
-cp ecofind ecogrep ecoisundertaxon ecoPCR ../..
-cd ../tools
-cp *.py ../..
-cd ../..
 # Add taxonomic information to 12S reference sequences
 perl adddummytaxid2fasta.pl < 12Sreferences.fasta > 12Sreferences.ecoPCR
 # Make ecoPCR database
@@ -24,28 +39,17 @@ python2.7 ./ecoPCRFormat.py -f -t ./taxonomy -n 12Sreferences 12Sreferences.ecoP
 # Run ecoPCR and convert to amplicon FASTA file
 ./ecoPCR -d 12Sreferences -e 5 -l 150 -L 250 GTCGGTAAAACTCGTGCCAGC CATAGTGGGGTATCTAATCCCAGTTTG | perl convertecopcr2fasta.pl -fp=GTCGGTAAAACTCGTGCCAGC -rp=CATAGTGGGGTATCTAATCCCAGTTTG | perl -npe 's/^>.+; />/' > 12Sbarcodes.fasta
 # Select representative sequences
-vsearch --fasta_width 0 --notrunclabels --threads 32 --cluster_fast 12Sbarcodes.fasta --qmask none --id 0.9 --strand plus --centroids 12Sbarcodes_representatives.fasta
+vsearch --fasta_width 0 --notrunclabels --threads 32 --cluster_fast 12Sbarcodes.fasta --qmask none --id 0.8 --strand plus --centroids 12Sbarcodes_representatives.fasta
 # Make fish community references for each sample
 perl makecommref.pl 20 4 50 10 12Sbarcodes_representatives.fasta
-# Download and compile Simera
-wget -c -O Simera-master.tar.gz https://github.com/bnichols1979/Simera/archive/refs/heads/master.tar.gz
-tar -xzf Simera-master.tar.gz
-patch Simera-master/Simera.h < Simera.diff
-cd Simera-master
-make clean
-make
-cd ..
+
 # Perform chimera formation simulation
-ls Sample??.fasta | grep -o -P '^[^\.]+' | xargs -P 32 -I {} sh -c './Simera-master/Simera -i {}.fasta -o {}_simeraout -n 30 -s 25000 -l 0.00005 -c 10000 -f GTCGGTAAAACTCGTGCCAGC -r CATAGTGGGGTATCTAATCCCAGTTTG'
-ls Blank??.fasta | grep -o -P '^[^\.]+' | xargs -P 32 -I {} sh -c './Simera-master/Simera -i {}.fasta -o {}_simeraout -n 30 -s 250 -l 0.00005 -c 10000 -f GTCGGTAAAACTCGTGCCAGC -r CATAGTGGGGTATCTAATCCCAGTTTG'
+ls Sample??.fasta | grep -o -P '^[^\.]+' | xargs -P 32 -I {} sh -c './Simera-master/Simera -i {}.fasta -o {}_simeraout -n 30 -s 10000 -l 0.00005 -c 10000 -f GTCGGTAAAACTCGTGCCAGC -r CATAGTGGGGTATCTAATCCCAGTTTG'
+ls Blank??.fasta | grep -o -P '^[^\.]+' | xargs -P 32 -I {} sh -c './Simera-master/Simera -i {}.fasta -o {}_simeraout -n 30 -s 100 -l 0.00005 -c 10000 -f GTCGGTAAAACTCGTGCCAGC -r CATAGTGGGGTATCTAATCCCAGTTTG'
 # Replace abundance format
 ls Sample??.fasta Blank??.fasta | grep -o -P '^[^\.]+' | xargs -P 32 -I {} sh -c 'perl -i -npe "if(/^>/){s/_(\\d+)\\n/\\;size=\$1\\n/;}" {}_simeraout/samp_all_seqs.fa'
 # Rereplicate sequences
 ls Sample??.fasta Blank??.fasta | grep -o -P '^[^\.]+' | xargs -P 32 -I {} sh -c 'vsearch --fasta_width 0 --notrunclabels --rereplicate {}_simeraout/samp_all_seqs.fa --sizein --xsize --output {}_simeraout/samp_rerep.fasta'
-# Download ART simulator
-wget -c https://www.niehs.nih.gov/research/resources/assets/docs/artbinmountrainier2016.06.05linux64.tgz
-# Extract ART simulator
-tar -xzf artbinmountrainier2016.06.05linux64.tgz
 # Generate simulated sequences
 ls Sample??.fasta Blank??.fasta | grep -o -P '^[^\.]+' | xargs -P 32 -I {} sh -c './art_bin_MountRainier/art_illumina --amplicon --seqSys MSv1 --in {}_simeraout/samp_rerep.fasta --len 144 --paired --noALN --fcov 1 --out {}_'
 # Make demultiplexed FASTQ
@@ -68,7 +72,7 @@ clsplitseq \
 --minqualtag=0 \
 --compress=disable \
 --numthreads=32 \
---seqnamestyle=illumina \
+--seqnamestyle=nochange \
 ../01a_RawSequences_woSTD/Undetermined_S0_L001_R1_001.fastq.gz \
 ../01a_RawSequences_woSTD/Undetermined_S0_L001_I1_001.fastq.gz \
 ../01a_RawSequences_woSTD/Undetermined_S0_L001_I2_001.fastq.gz \
@@ -83,10 +87,11 @@ cd ..
 rm -rf TEMP
 
 # Make "wSTD" sequences
-ls Sample??.fasta Blank??.fasta | grep -o -P '^[^\.]+' | xargs -P 32 -I {} sh -c 'cat {}.fasta MiFish_STD_*.fasta > {}_wSTD.fasta'
+ls Sample??.fasta | grep -o -P '^[^\.]+' | xargs -P 32 -I {} sh -c 'cat {}.fasta MiFish_STD_*.fasta > {}_wSTD.fasta'
+ls Blank??.fasta | grep -o -P '^[^\.]+' | xargs -P 32 -I {} sh -c 'cat {}.fasta > {}_wSTD.fasta; cat MiFish_STD_*.fasta | perl -npe "if(/^>/){s/\$/0/}" >> {}_wSTD.fasta'
 # Perform chimera formation simulation
-ls Sample??_wSTD.fasta | grep -o -P '^[^\.]+' | xargs -P 32 -I {} sh -c './Simera-master/Simera -i {}.fasta -o {}_simeraout -n 30 -s 50000 -l 0.00005 -c 10000 -f GTCGGTAAAACTCGTGCCAGC -r CATAGTGGGGTATCTAATCCCAGTTTG'
-ls Blank??_wSTD.fasta | grep -o -P '^[^\.]+' | xargs -P 32 -I {} sh -c './Simera-master/Simera -i {}.fasta -o {}_simeraout -n 30 -s 500 -l 0.00005 -c 10000 -f GTCGGTAAAACTCGTGCCAGC -r CATAGTGGGGTATCTAATCCCAGTTTG'
+ls Sample??_wSTD.fasta | grep -o -P '^[^\.]+' | xargs -P 32 -I {} sh -c './Simera-master/Simera -i {}.fasta -o {}_simeraout -n 30 -s 20000 -l 0.00005 -c 10000 -f GTCGGTAAAACTCGTGCCAGC -r CATAGTGGGGTATCTAATCCCAGTTTG'
+ls Blank??_wSTD.fasta | grep -o -P '^[^\.]+' | xargs -P 32 -I {} sh -c './Simera-master/Simera -i {}.fasta -o {}_simeraout -n 30 -s 10000 -l 0.00005 -c 10000 -f GTCGGTAAAACTCGTGCCAGC -r CATAGTGGGGTATCTAATCCCAGTTTG'
 # Replace abundance format
 ls Sample??_wSTD.fasta Blank??_wSTD.fasta | grep -o -P '^[^\.]+' | xargs -P 32 -I {} sh -c 'perl -i -npe "if(/^>/){s/_(\\d+)\\n/\\;size=\$1\\n/;}" {}_simeraout/samp_all_seqs.fa'
 # Rereplicate sequences
@@ -117,7 +122,7 @@ clsplitseq \
 --minqualtag=0 \
 --compress=disable \
 --numthreads=32 \
---seqnamestyle=illumina \
+--seqnamestyle=nochange \
 ../01b_RawSequences_wSTD/Undetermined_S0_L001_R1_001.fastq.gz \
 ../01b_RawSequences_wSTD/Undetermined_S0_L001_I1_001.fastq.gz \
 ../01b_RawSequences_wSTD/Undetermined_S0_L001_I2_001.fastq.gz \
