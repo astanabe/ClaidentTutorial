f79b15c07f438cf6096929a89a50943de74ac413
astanabe <akifumi.tanabe@gmail.com>
2025-05-07 07:47:02 +0900

    A sequence generation script has been updated.

--- 00_GenerateSequences/GenerateSequences.sh
+++ 00_GenerateSequences/GenerateSequences.sh
@@ -1,9 +1,18 @@
+CURDIR=`pwd`
+NCPU=`grep -c processor /proc/cpuinfo`
 # Install several packages
-sudo apt install libgsl-dev libmath-random-mt-perl python2.7
+sudo apt install build-essential checkinstall libncursesw5-dev libssl-dev libsqlite3-dev tk-dev libgdbm-dev libc6-dev libbz2-dev libffi-dev libgsl-dev libmath-random-mt-perl wget unzip
+# Install Python 2.7.18
+wget https://www.python.org/ftp/python/2.7.18/Python-2.7.18.tgz
+tar -xzf Python-2.7.18.tgz
+cd Python-2.7.18
+./configure --enable-optimizations --prefix=$CURDIR/Python-2.7.18
+make -j$NCPU
+make install
 # Install ecoPCR
 tar -xzf ecoPCR-0.8.0.tar.gz
 cd ecoPCR-0.8.0/src
-make -j4
+make -j$NCPU
 make ecoisundertaxon
 cp ecofind ecogrep ecoisundertaxon ecoPCR ../..
 cd ../tools
@@ -25,35 +34,32 @@ tar -xzf artbinmountrainier2016.06.05linux64.tgz
 # Download taxonomy dump file and extract
 mkdir -p taxonomy
 cd taxonomy
-wget -c https://ftp.ncbi.nih.gov/pub/taxonomy/taxdump.tar.gz
-wget -c https://ftp.ncbi.nih.gov/pub/taxonomy/taxdump.tar.gz.md5
-md5sum -c taxdump.tar.gz.md5 || exit $?
-tar -xzf taxdump.tar.gz
-chmod 644 taxdump.tar.gz
-rm taxdump.tar.gz
+wget -c https://ftp.ncbi.nih.gov/pub/taxonomy/taxdump_archive/taxdmp_2023-01-01.zip
+unzip taxdmp_2023-01-01.zip -d taxonomy
+rm taxdmp_2023-01-01.zip
 cd ..
 # Add taxonomic information to 12S reference sequences
 perl adddummytaxid2fasta.pl < 12Sreferences.fasta > 12Sreferences.ecoPCR
 # Make ecoPCR database
-python2.7 ./ecoPCRFormat.py -f -t ./taxonomy -n 12Sreferences 12Sreferences.ecoPCR
+Python-2.7.18/bin/python2.7 ./ecoPCRFormat.py -f -t ./taxonomy -n 12Sreferences 12Sreferences.ecoPCR
 # Run ecoPCR and convert to amplicon FASTA file
 ./ecoPCR -d 12Sreferences -e 5 -l 150 -L 250 GTCGGTAAAACTCGTGCCAGC CATAGTGGGGTATCTAATCCCAGTTTG | perl convertecopcr2fasta.pl -fp=GTCGGTAAAACTCGTGCCAGC -rp=CATAGTGGGGTATCTAATCCCAGTTTG | perl -npe 's/^>.+; />/' > 12Sbarcodes.fasta
 # Select representative sequences
-vsearch --fasta_width 0 --notrunclabels --threads 32 --cluster_fast 12Sbarcodes.fasta --qmask none --id 0.9 --strand plus --centroids 12Sbarcodes_representatives.fasta
+vsearch --fasta_width 0 --notrunclabels --threads $NCPU --cluster_fast 12Sbarcodes.fasta --qmask none --id 0.9 --strand plus --centroids 12Sbarcodes_representatives.fasta
 # Make fish community references for each sample
 perl makecommref.pl 20 4 50 10 12Sbarcodes_representatives.fasta
 
 # Perform chimera formation simulation
-ls Sample??.fasta | grep -o -P '^[^\.]+' | xargs -P 32 -I {} sh -c './Simera-master/Simera -i {}.fasta -o {}_simeraout -n 30 -s 10000 -l 0.00005 -c 10000 -f GTCGGTAAAACTCGTGCCAGC -r CATAGTGGGGTATCTAATCCCAGTTTG'
-ls Blank??.fasta | grep -o -P '^[^\.]+' | xargs -P 32 -I {} sh -c './Simera-master/Simera -i {}.fasta -o {}_simeraout -n 30 -s 100 -l 0.00005 -c 10000 -f GTCGGTAAAACTCGTGCCAGC -r CATAGTGGGGTATCTAATCCCAGTTTG'
+ls Sample??.fasta | grep -o -P '^[^\.]+' | xargs -P $NCPU -I {} sh -c './Simera-master/Simera -i {}.fasta -o {}_simeraout -n 50 -s 10000 -l 0.0001 -c 10000 -f GTCGGTAAAACTCGTGCCAGC -r CATAGTGGGGTATCTAATCCCAGTTTG'
+ls Blank??.fasta | grep -o -P '^[^\.]+' | xargs -P $NCPU -I {} sh -c './Simera-master/Simera -i {}.fasta -o {}_simeraout -n 50 -s 100 -l 0.0001 -c 10000 -f GTCGGTAAAACTCGTGCCAGC -r CATAGTGGGGTATCTAATCCCAGTTTG'
 # Replace abundance format
-ls Sample??.fasta Blank??.fasta | grep -o -P '^[^\.]+' | xargs -P 32 -I {} sh -c 'perl -i -npe "if(/^>/){s/_(\\d+)\\n/\\;size=\$1\\n/;}" {}_simeraout/samp_all_seqs.fa'
+ls Sample??.fasta Blank??.fasta | grep -o -P '^[^\.]+' | xargs -P $NCPU -I {} sh -c 'perl -i -npe "if(/^>/){s/_(\\d+)\\n/\\;size=\$1\\n/;}" {}_simeraout/samp_all_seqs.fa'
 # Rereplicate sequences
-ls Sample??.fasta Blank??.fasta | grep -o -P '^[^\.]+' | xargs -P 32 -I {} sh -c 'vsearch --fasta_width 0 --notrunclabels --rereplicate {}_simeraout/samp_all_seqs.fa --sizein --xsize --output {}_simeraout/samp_rerep.fasta'
+ls Sample??.fasta Blank??.fasta | grep -o -P '^[^\.]+' | xargs -P $NCPU -I {} sh -c 'vsearch --fasta_width 0 --notrunclabels --rereplicate {}_simeraout/samp_all_seqs.fa --sizein --xsize --output {}_simeraout/samp_rerep.fasta'
 # Generate simulated sequences
-ls Sample??.fasta Blank??.fasta | grep -o -P '^[^\.]+' | xargs -P 32 -I {} sh -c './art_bin_MountRainier/art_illumina --amplicon --seqSys MSv1 --in {}_simeraout/samp_rerep.fasta --len 144 --paired --noALN --fcov 1 --out {}_'
+ls Sample??.fasta Blank??.fasta | grep -o -P '^[^\.]+' | xargs -P $NCPU -I {} sh -c './art_bin_MountRainier/art_illumina --amplicon --seqSys MSv1 --in {}_simeraout/samp_rerep.fasta --len 144 --paired --noALN --fcov 1 --out {}_'
 # Make demultiplexed FASTQ
-ls Sample??.fasta Blank??.fasta | grep -o -P '^[^\.]+' | xargs -P 32 -I {} sh -c 'perl addNNNNNN.pl {}_1.fq > {}_R1_001.fastq; perl addNNNNNN.pl {}_2.fq > {}_R2_001.fastq'
+ls Sample??.fasta Blank??.fasta | grep -o -P '^[^\.]+' | xargs -P $NCPU -I {} sh -c 'perl addNNNNNN.pl {}_1.fq > {}_R1_001.fastq; perl addNNNNNN.pl {}_2.fq > {}_R2_001.fastq'
 # Make output folder
 mkdir -p ../01a_RawSequences_woSTD
 # Make 2 index sequence files
@@ -71,7 +77,7 @@ clsplitseq \
 --index2file=../index2.fasta \
 --minqualtag=0 \
 --compress=disable \
---numthreads=32 \
+--numthreads=$NCPU \
 --seqnamestyle=nochange \
 ../01a_RawSequences_woSTD/Undetermined_S0_L001_R1_001.fastq.gz \
 ../01a_RawSequences_woSTD/Undetermined_S0_L001_I1_001.fastq.gz \
@@ -81,25 +87,25 @@ TEMP
 # Move demultiplexed files
 cd TEMP
 ls *.fastq | perl -nle '$fn=$_;m/TEMP__(.+)__TEMP\.(forward|reverse)\.fastq/;$sn=$1;$fr=$2;if($sn=~/^[ACGT]+\+[ACGT]+$/){unlink($fn)}else{if($fr eq "forward"){rename($fn,"$sn\_R1_001.fastq")}else{rename($fn,"$sn\_R2_001.fastq")}}'
-ls *.fastq | xargs -L 1 -P 32 gzip -9
+ls *.fastq | xargs -L 1 -P $NCPU gzip -9
 mv *.fastq.gz ../../01a_RawSequences_woSTD/
 cd ..
 rm -rf TEMP
 
 # Make "wSTD" sequences
-ls Sample??.fasta | grep -o -P '^[^\.]+' | xargs -P 32 -I {} sh -c 'cat {}.fasta MiFish_STD_*.fasta > {}_wSTD.fasta'
-ls Blank??.fasta | grep -o -P '^[^\.]+' | xargs -P 32 -I {} sh -c 'cat {}.fasta > {}_wSTD.fasta; cat MiFish_STD_*.fasta | perl -npe "if(/^>/){s/\$/0/}" >> {}_wSTD.fasta'
+ls Sample??.fasta | grep -o -P '^[^\.]+' | xargs -P $NCPU -I {} sh -c 'cat {}.fasta MiFish_STD_*.fasta > {}_wSTD.fasta'
+ls Blank??.fasta | grep -o -P '^[^\.]+' | xargs -P $NCPU -I {} sh -c 'cat {}.fasta > {}_wSTD.fasta; cat MiFish_STD_*.fasta | perl -npe "if(/^>/){s/\$/0/}" >> {}_wSTD.fasta'
 # Perform chimera formation simulation
-ls Sample??_wSTD.fasta | grep -o -P '^[^\.]+' | xargs -P 32 -I {} sh -c './Simera-master/Simera -i {}.fasta -o {}_simeraout -n 30 -s 20000 -l 0.00005 -c 10000 -f GTCGGTAAAACTCGTGCCAGC -r CATAGTGGGGTATCTAATCCCAGTTTG'
-ls Blank??_wSTD.fasta | grep -o -P '^[^\.]+' | xargs -P 32 -I {} sh -c './Simera-master/Simera -i {}.fasta -o {}_simeraout -n 30 -s 10000 -l 0.00005 -c 10000 -f GTCGGTAAAACTCGTGCCAGC -r CATAGTGGGGTATCTAATCCCAGTTTG'
+ls Sample??_wSTD.fasta | grep -o -P '^[^\.]+' | xargs -P $NCPU -I {} sh -c './Simera-master/Simera -i {}.fasta -o {}_simeraout -n 50 -s 20000 -l 0.0001 -c 10000 -f GTCGGTAAAACTCGTGCCAGC -r CATAGTGGGGTATCTAATCCCAGTTTG'
+ls Blank??_wSTD.fasta | grep -o -P '^[^\.]+' | xargs -P $NCPU -I {} sh -c './Simera-master/Simera -i {}.fasta -o {}_simeraout -n 50 -s 10000 -l 0.0001 -c 10000 -f GTCGGTAAAACTCGTGCCAGC -r CATAGTGGGGTATCTAATCCCAGTTTG'
 # Replace abundance format
-ls Sample??_wSTD.fasta Blank??_wSTD.fasta | grep -o -P '^[^\.]+' | xargs -P 32 -I {} sh -c 'perl -i -npe "if(/^>/){s/_(\\d+)\\n/\\;size=\$1\\n/;}" {}_simeraout/samp_all_seqs.fa'
+ls Sample??_wSTD.fasta Blank??_wSTD.fasta | grep -o -P '^[^\.]+' | xargs -P $NCPU -I {} sh -c 'perl -i -npe "if(/^>/){s/_(\\d+)\\n/\\;size=\$1\\n/;}" {}_simeraout/samp_all_seqs.fa'
 # Rereplicate sequences
-ls Sample??_wSTD.fasta Blank??_wSTD.fasta | grep -o -P '^[^\.]+' | xargs -P 32 -I {} sh -c 'vsearch --fasta_width 0 --notrunclabels --rereplicate {}_simeraout/samp_all_seqs.fa --sizein --xsize --output {}_simeraout/samp_rerep.fasta'
+ls Sample??_wSTD.fasta Blank??_wSTD.fasta | grep -o -P '^[^\.]+' | xargs -P $NCPU -I {} sh -c 'vsearch --fasta_width 0 --notrunclabels --rereplicate {}_simeraout/samp_all_seqs.fa --sizein --xsize --output {}_simeraout/samp_rerep.fasta'
 # Generate simulated sequences
-ls Sample??_wSTD.fasta Blank??_wSTD.fasta | grep -o -P '^[^\.]+' | xargs -P 32 -I {} sh -c './art_bin_MountRainier/art_illumina --amplicon --seqSys MSv1 --in {}_simeraout/samp_rerep.fasta --len 144 --paired --noALN --fcov 1 --out {}_'
+ls Sample??_wSTD.fasta Blank??_wSTD.fasta | grep -o -P '^[^\.]+' | xargs -P $NCPU -I {} sh -c './art_bin_MountRainier/art_illumina --amplicon --seqSys MSv1 --in {}_simeraout/samp_rerep.fasta --len 144 --paired --noALN --fcov 1 --out {}_'
 # Make demultiplexed FASTQ
-ls Sample??_wSTD.fasta Blank??_wSTD.fasta | grep -o -P '^[^\.]+' | xargs -P 32 -I {} sh -c 'perl addNNNNNN.pl {}_1.fq > {}_R1_001.fastq; perl addNNNNNN.pl {}_2.fq > {}_R2_001.fastq'
+ls Sample??_wSTD.fasta Blank??_wSTD.fasta | grep -o -P '^[^\.]+' | xargs -P $NCPU -I {} sh -c 'perl addNNNNNN.pl {}_1.fq > {}_R1_001.fastq; perl addNNNNNN.pl {}_2.fq > {}_R2_001.fastq'
 # Make output folder
 mkdir -p ../01b_RawSequences_wSTD
 # Make temporary index files
@@ -121,7 +127,7 @@ clsplitseq \
 --index2file=../index2.fasta \
 --minqualtag=0 \
 --compress=disable \
---numthreads=32 \
+--numthreads=$NCPU \
 --seqnamestyle=nochange \
 ../01b_RawSequences_wSTD/Undetermined_S0_L001_R1_001.fastq.gz \
 ../01b_RawSequences_wSTD/Undetermined_S0_L001_I1_001.fastq.gz \
@@ -131,7 +137,7 @@ TEMP
 # Move demultiplexed files
 cd TEMP
 ls *.fastq | perl -nle '$fn=$_;m/TEMP__(.+)__TEMP\.(forward|reverse)\.fastq/;$sn=$1;$fr=$2;if($sn=~/^[ACGT]+\+[ACGT]+$/){unlink($fn)}else{if($fr eq "forward"){rename($fn,"$sn\_R1_001.fastq")}else{rename($fn,"$sn\_R2_001.fastq")}}'
-ls *.fastq | xargs -L 1 -P 32 gzip -9
+ls *.fastq | xargs -L 1 -P $NCPU gzip -9
 mv *.fastq.gz ../../01b_RawSequences_wSTD/
 cd ..
 rm -rf TEMP
